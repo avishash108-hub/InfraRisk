8a2423b2c5e856ec4d88103da1a3e68df378cd22
avishash108-hub <avishash108@gmail.com>
2026-08-28 00:09:09 +0530

    Update pwd.py

--- backend/pwd.py
+++ backend/pwd.py
@@ -1,9 +1,12 @@
+#GENERATING PASSWORDS USING ARGON2
+
+
 from argon2 import PasswordHasher
 ph = PasswordHasher(time_cost = 3, memory_cost = 65536, parallelism = 4)
 #pwd = ph.hash('officer001')
 #pwd = ph.hash('officer002')
 #pwd = ph.hash('fieldinsp001')
-pwd = ph.hash('fieldinsp002')
+#pwd = ph.hash('fieldinsp002')
 pwd = ph.hash('fieldinsp003')
 
 print(pwd)
