538b036e56af23058acc17a30cb6f61129cbdfc2
avishash108-hub <avishash108@gmail.com>
2026-08-28 00:08:39 +0530

    Update pwd.py

--- backend/pwd.py
+++ backend/pwd.py
@@ -3,7 +3,7 @@ ph = PasswordHasher(time_cost = 3, memory_cost = 65536, parallelism = 4)
 #pwd = ph.hash('officer001')
 #pwd = ph.hash('officer002')
 #pwd = ph.hash('fieldinsp001')
-#pwd = ph.hash('fieldinsp002')
+pwd = ph.hash('fieldinsp002')
 pwd = ph.hash('fieldinsp003')
 
 print(pwd)
